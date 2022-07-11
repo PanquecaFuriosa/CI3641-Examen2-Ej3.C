6dc87dda9d855787ce242f1c521ea7873798cb82
PanquecaFuriosa <18-10761@usb.ve>
2022-07-11 01:13:31 -0400

    doc

--- BienParentizadas.py
+++ BienParentizadas.py
@@ -1,10 +1,28 @@
 def ins(e, ls):
+    """Función que determina todas las maneras de insertar un elemento en una lista
+
+    Args:
+        e (any): Elemento cualquiera a ser insertado.
+        ls (list): Lista a la cual se le insertará e.
+
+    Yields:
+        lis: Lista con el elemento insertado en alguna posición.
+    """
     yield [e, *ls]
     if ls:
         for i in ins(e, ls[1:]):
             yield [ls[0], *i]
 
 def misterio(ls):
+    """Función que determina las posibles permutaciones de los elementos
+    de un arreglo.
+
+    Args:
+        ls (list): Lista a determinar las posibles permutaciones.
+
+    Yields:
+        list: Lista con una permutación.
+    """
     if ls:
         for m in misterio(ls[1:]):
             for i in ins(ls[0], m):
@@ -14,6 +32,15 @@ def misterio(ls):
         yield []
 
 def bienParentizadas(n):
+    """Función que determina las posibles combinaciones
+    de un número n de paréntesis, de manera válida.
+
+    Args:
+        n (int): Número de pares de paréntesis a permutar.
+
+    Yields:
+        str: Una permutación bien patentizada.
+    """
     lista = []
     for m in misterio(["(" for i in range(n)]+[")" for i in range(n)]):
         a = 0
